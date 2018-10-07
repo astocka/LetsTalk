eaea3d0bb5b3cf71f6515823c27cf15d8bb9d5cc
Aleksandra Stocka <aleksandra.stocka689@gmail.com>
2018-10-07 23:02:39 +0200

    add ApplicationDbContext with settings

---
+++ SignalRChatApp/SignalRChatApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SignalRChatApp.Models;
+using SignalRChatApp.Areas.Identity.Models;
+
+namespace SignalRChatApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        //public DbSet<Client> Clients { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    builder.Entity<Client>()
+        //        .HasIndex(c => c.Name).IsUnique();
+        //    base.OnModelCreating(builder);
+        //}
+    }
+}
--- SignalRChatApp/SignalRChatApp/Startup.cs
+++ SignalRChatApp/SignalRChatApp/Startup.cs
@@ -6,8 +6,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SignalRChatApp.Areas.Identity.Models;
+using SignalRChatApp.Data;
 using SignalRChatApp.Hubs;
 
 namespace SignalRChatApp
@@ -31,6 +34,11 @@ namespace SignalRChatApp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<User>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
@@ -43,14 +51,18 @@ namespace SignalRChatApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseAuthentication();
+
             app.UseSignalR(routes =>
             {
                 routes.MapHub<ChatHub>("/chat");
