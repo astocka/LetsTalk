be76b198c5cee5d43d66945f87b3e3ad7867bfdf
Aleksandra Stocka <aleksandra.stocka689@gmail.com>
2018-10-07 23:00:21 +0200

    add Areas

---
+++ SignalRChatApp/SignalRChatApp/Areas/Identity/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using SignalRChatApp.Models;
+
+namespace SignalRChatApp.Areas.Identity.Models
+{
+	public class User : IdentityUser<Guid>
+	{
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Picture { get; set; }
+	}
+}
